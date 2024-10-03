bed2f3ef7b74909bae3b684874010d55a4410f42
Ekaterina Khomik <Normik2605@gmail.com>
2024-10-03 10:57:22 +0300

    Страница профиля

--- lib/pages/Profile_page.dart
+++ lib/pages/Profile_page.dart
@@ -9,7 +9,59 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: const Center(child: Text('Профиль')),
       ),
-    body: 
-    Center(child: Text('Профиль'),),);
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profile/unknown.jpg'), 
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Дмитрий Куплинов',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'D.Kupl1n0v@example.com',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Личная информация'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Настройки'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Уведомления'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Помощь'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Выйти'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
