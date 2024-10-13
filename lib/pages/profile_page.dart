68adec22e2be21b39a0259b0c721de2aa45fd43c
Ekaterina Khomik <Normik2605@gmail.com>
2024-10-13 21:21:15 +0300

    Заглушка для профиля

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  final String name = 'Хомик Екатерина Андреевна';
+  final String group = 'ЭФБО-06-22';
+  final String phoneNumber = '+7 (123) 456-78-90';
+  final String email = 'example@gmail.com';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,26 +21,22 @@ class ProfilePage extends StatelessWidget {
           children: [
             const Text('ФИО', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
-
-            const Text('Хомик Екатерина Андреевна', style: TextStyle(fontSize: 16)),
+            Text(name, style: const TextStyle(fontSize: 16)),
             const SizedBox(height: 16),
 
             const Text('Группа', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
-
-            const Text('ЭФБО-06-22', style: TextStyle(fontSize: 16)),
+            Text(group, style: const TextStyle(fontSize: 16)),
             const SizedBox(height: 16),
 
             const Text('Номер телефона', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
-
-            const Text('+7 (123) 456-78-90', style: TextStyle(fontSize: 16)),
+            Text(phoneNumber, style: const TextStyle(fontSize: 16)),
             const SizedBox(height: 16),
 
             const Text('Почта', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
-
-            const Text('example@gmail.com', style: TextStyle(fontSize: 16)),
+            Text(email, style: const TextStyle(fontSize: 16)),
           ],
         ),
       ),
